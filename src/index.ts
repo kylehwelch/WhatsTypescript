44e093cafa8999f447eae397769fc676a6a6ba71
kylehwelch <welch@udel.edu>
2022-08-13 17:32:54 -0400

    saving little things

--- src/index.ts
+++ src/index.ts
@@ -6,19 +6,21 @@ const form: HTMLFormElement = document.querySelector('#defineform');
 
 
 form.onsubmit = () => {
+  event.preventDefault();
   const formData = new FormData(form);
 
   console.log(formData);
   const text = formData.get('defineword') as string;
+  fetchJSON(text);
   console.log(text);
   return false; // prevent reload
 };
 
-
-// function fetchDefinition(word: string) {
-//   return fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`)
-//       .then(response => response.json())
-// }
-
-
-
+function fetchJSON(text: string) {
+  fetch('https://api.dictionaryapi.dev/api/v2/entries/en/' + text)
+  .then(response => response.json())
+  .then(data => {
+      console.log(data);
+      localStorage.setItem('definition', JSON.stringify(data));
+  });
+}
