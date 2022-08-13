f468a5e4ede1b68250df1fef6bde5faaba531d41
kylehwelch <welch@udel.edu>
2022-08-13 16:19:50 -0400

    add and delete

--- fetchJSON.ts
+++
@@ -1,4 +0,0 @@
-function fetchJSON(word: string) {
-    return fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`)
-        .then(response => response.json())
-}
---
+++ randoFunctions.ts
@@ -0,0 +1,25 @@
+// const data = [ { "definition": "an expression of greeting", "word": "hello" }, { "definition": "a telephone call", "word": "hello" } ]; 
+
+// const definitions = data.map(entry => entry.definition); 
+
+// const words = data.map(entry => entry.word); 
+
+// console.log(definitions); // ['an expression of greeting', 'a telephone call'] console.log(words); // ['hello', 'hello']
+
+
+// -------
+
+
+// const word: string = "test"; 
+
+// const definition: string = "a procedure or test to determine the quality, performance, or reliability of something"; 
+
+// const apiUrl: string = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`; 
+
+// const response = await fetch(apiUrl); 
+
+// const data = await response.json(); 
+
+// const word: string = data.word; 
+
+// const definition: string = data.definition;
